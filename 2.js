4b0e112cfbe95f4acf6b3668fe074c5fd905d35e
RajAditya01 <adityavishal903@gmail.com>
2023-07-10 22:03:35 +0530

    2

---
+++ 2.js
@@ -0,0 +1,2 @@
+const fs = require("fs");
+fs.writeFileSync("read.txt", "hii i am aditya");
--- index.js
+++ index.js
@@ -4,3 +4,5 @@
 //3: Multiline codes
 //4: use(_) to get the last result
 //5: we can use editor mode
+const fs=require("fs");
+fs.writeFilesync("read.txt","hii i am aditya");
