fd90fd7ee43cca52c1aac024f8e5c94e5f1fa022
RajAditya01 <adityavishal903@gmail.com>
2023-07-11 12:18:06 +0530

    .

--- fsasync/index.js
+++ fsasync/index.js
@@ -1,6 +1,15 @@
 const fs=require("fs");
+const { start } = require("repl");
 
 fs.writeFile("read.txt","today is awsome day :)",(err) =>{
     console.log("files is created");
     console.log(err);
 } );
+
+
+// we pass them a function as an arrugunment - a callback -
+// that get called when that task completes.
+// The callback has an arrugunment that tells you wheater
+// the operation complete successfully.TheNow we need to say what to do when fs.eritefile
+// has completed (even if its nothing) and start
+// checking for errors.
