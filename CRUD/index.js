bdaf71399305e60305e03a0334748b3b729c6bb4
RajAditya01 <adityavishal903@gmail.com>
2023-07-11 10:31:03 +0530

    .

---
+++ CRUD/index.js
@@ -0,0 +1,8 @@
+//CRUD
+const fs = require("fs")
+fs.writeFileSync("aditya.txt" , "hii this is me :)");
+fs.appendFileSync("aditya.txt" , " this is append file");
+
+const fs = require('fs');
+const readline = require('readline');
+fs.rename("aditya.txt", "raj.txt",)
