a253ac76406c312135340696b1983b09dca9dd07
RajAditya01 <adityavishal903@gmail.com>
2023-07-11 11:53:06 +0530

    .

--- 2.js
+++ 2.js
@@ -7,3 +7,7 @@ const bufData=fs.readFileSync("read.txt");
 console.log(bufData);
 //output
 //<Buffer 68 69 69 20 69 20 61 6d 20 61 64 69 74 79 61 20 68 6f 77 20 61 72 65 20 79 6f 75 20 69 20 61 6d 20 66 69 6e 65 20 74 68 61 6e 6b 75 68>
+
+fs.renamesync("aditya.txt", "raj.txt");
+//delete
+fs.unlinkSync("aditya.txt");
--- CRUD/index.js
+++ CRUD/index.js
@@ -1,10 +1,10 @@
 //CRUD
 const fs = require("fs");
-fs.mkdirSync("CRUD");
-fs.writeFileSync("CRUD/aditya.txt", "hii this is me :)");
-fs.appendFileSync("CRUD/aditya.txt", " this is append file");
-const bufData = fs.readFileSync("CRUD/aditya.txt", "utf8");
+//fs.mkdirSync("CRUD");
+fs.writeFileSync("aditya.txt", "hii this is me :)");
+fs.appendFileSync("aditya.txt", " this is append file");
+const bufData = fs.readFileSync("aditya.txt", "utf8");
 
-// const fs = require('fs');
-// const readline = require('readline');
-// fs.rename("aditya.txt", "raj.txt",)
+fs.renamesync("aditya.txt", "raj.txt");
+//delete
+fs.unlinkSync("aditya.txt");
