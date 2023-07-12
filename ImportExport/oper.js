9bc18bda3e061b846162de4ea77733dedabfa8e1
RajAditya01 <adityavishal903@gmail.com>
2023-07-12 22:34:29 +0530

    .

--- ImportExport/oper.js
+++ ImportExport/oper.js
@@ -6,7 +6,14 @@ const sub = (a,b) =>{
     return a - b;
 };
 
+const mult =(a,b) =>{
+    return a * b;
+}
+
 const name = "vinod";
 
-module.exports=add;
-module.exports=sub;
+// module.exports=add;
+// module.exports=sub;
+// module.exports=mult;
+
+module.exports= {add, sub, mult,name};
