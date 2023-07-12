647534f056795f965f311c5f70364de54df1a2ab
RajAditya01 <adityavishal903@gmail.com>
2023-07-12 12:19:20 +0530

    .

--- chalange2/nodech2.js
+++ chalange2/nodech2.js
@@ -18,13 +18,17 @@ fs.readFile("raj.txt", "utf-8", (err,data)=>{
 //rename
 fs.rename("raj.txt", "rajaditya.txt", (err)=>{
     console.log("rename Done!");
-})
+});
 //file delete
 fs.unlink("rajaditya.txt", (err)=>{
     console.log("file deleted!")
-})
+});
 
+//create folder
+fs.mkdir('aditya',(err)=>{
+    console.log("folder created!");
+});
 //folder delete
 fs.rmdir("name of folder",(err)=>{
     console.log("folder deleted!")
-})
+});
