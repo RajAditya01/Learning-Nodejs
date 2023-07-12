782b16b5ad5bee124e05109adb29462f367a5d0d
RajAditya01 <adityavishal903@gmail.com>
2023-07-13 00:28:20 +0530

    .

---
+++ HTTPserver/index.js
@@ -0,0 +1,9 @@
+// const { request } = require("http")
+
+// The http.createServer() method includes request and response which is supplied by Node.js.Node
+// e.g -> url, request header , and data.
+
+// The request Object can be used to get information about the current HTTP request
+
+// if the response object can be used to send a response for a current HTTP server is supposed to be displayed as HTMLAllCollection,
+// you should include an http header with the correct content type:
