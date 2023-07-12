c2b9bd1a499bc995c4eb1c2649c27e60bd9f67fa
RajAditya01 <adityavishal903@gmail.com>
2023-07-12 15:04:11 +0530

    .

--- PathModule/index.js
+++ PathModule/index.js
@@ -1,3 +1,7 @@
 const path = require("Path");
 
-console.log(path.dirname('C:/Users/adity/OneDrive/Desktop/Learning-Nodejs/PathModule/index.js'));
+console.log(path.dirname("C:/Users/adity/OneDrive/Desktop/Learning-Nodejs/PathModule/index.js"));
+console.log(path.extname("C:/Users/adity/OneDrive/Desktop/Learning-Nodejs/PathModule/index.js"));
+console.log(path.basename("C:/Users/adity/OneDrive/Desktop/Learning-Nodejs/PathModule/index.js"));
+
+console.log(path.parse("C:/Users/adity/OneDrive/Desktop/Learning-Nodejs/PathModule/index.js"));
