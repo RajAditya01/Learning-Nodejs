4c7d91f8c783f363d7ed5c9c89f7453a4d342282
RajAditya01 <adityavishal903@gmail.com>
2023-07-12 23:26:46 +0530

    .

--- NPM-Mod/index.js
+++
@@ -1,7 +0,0 @@
-import('chalk').then((chalk) => {
-    // Use chalk here
-  }).catch((error) => {
-    // Handle any errors
-  });
-
-console.log(chalk.blue('Hello world!'));
---
+++ NPM-Mod/tempCodeRunnerFile.mjs
@@ -0,0 +1,5 @@
+const validator= require("validator");
+
+// const res = validator.isEmail("adityavishal903@gmail.com");
+// console.log(res);
+
