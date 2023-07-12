aa8663a0f1626c24da406f2344ff402c36c34ff6
RajAditya01 <adityavishal903@gmail.com>
2023-07-13 00:13:23 +0530

    .

---
+++ ModuleWrapperFun/index.js
@@ -0,0 +1,13 @@
+(function (exports, require, module, __filename, __dirname) {
+    //module code
+    const a = require("fs");
+    const name = "Aditya";
+    console,log(name);
+    module.exports ={exporthhscvyuv}
+  });
+
+//   Whai is IIFE?
+(function(){
+    var a="aditya";
+    console.log(a);//isko bahr use ni kr skte
+})();
