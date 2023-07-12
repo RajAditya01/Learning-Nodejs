05951d5b63202c575a6e6561de8678656b52f6c8
RajAditya01 <adityavishal903@gmail.com>
2023-07-12 23:29:23 +0530

    .

--- NPM-Mod/index.mjs
+++ NPM-Mod/index.mjs
@@ -1,9 +1,9 @@
 import chalk from "chalk";
 import validator from 'validator';
 
-console.log(chalk.blue("Hello world!"));
+//console.log(chalk.blue("Hello world!"));
 
 
 const res = validator.isEmail("adityavishal903@gmail.com");
-console.log(res);
+console.log(res ? chalk.green.inverse(res) : chalk.red.inverse(res));
 
