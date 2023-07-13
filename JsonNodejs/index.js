f83ce4c00f26f8c6ce3be96f12e2067bb8b8fbde
RajAditya01 <adityavishal903@gmail.com>
2023-07-13 14:53:06 +0530

    .

--- JsonNodejs/index.js
+++ JsonNodejs/index.js
@@ -2,4 +2,15 @@
 // json is lighweight format for storing and transporting DataTransfer.
 // format for storing and transporting DataTransfer.
 // json is often used when data is sent from a server to web.
+const fs =require("fs");
+const bioData ={
+    name : "Aditya",
+    age : 20,
+    channel: "Aditya technical",
+}
 
+//convert to jeson
+const jsonData = JSON.stringify(bioData);
+fs.writeFile("json1.json" , jsonData, (err) => {
+    console.log("Done!");
+});
