ddda1aacf3fbf8063f1b5b0fa86bce98f4ccd844
RajAditya01 <adityavishal903@gmail.com>
2023-07-13 09:46:25 +0530

    .

--- HTTPserver/index.js
+++ HTTPserver/index.js
@@ -6,4 +6,15 @@
 // The request Object can be used to get information about the current HTTP request
 
 // if the response object can be used to send a response for a current HTTP server is supposed to be displayed as HTMLAllCollection,
+
 // you should include an http header with the correct content type:
+
+const http = require("http");
+
+const server = http.createServer((req , res) => {
+    res.end("Hello from the other sides");
+});
+
+server.listen(8000, "127.0.0.1" , () => {
+    console.log("listening to the port no 8000");
+});
