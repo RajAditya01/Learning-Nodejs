4c7d8879fec2852a68a25a6001c864ecf5898bfc
RajAditya01 <adityavishal903@gmail.com>
2023-07-13 11:40:28 +0530

    .

--- Json/index.js
+++
@@ -1,3 +0,0 @@
-// json stands for javascript object notation.json is lighweight format for storing and transporting DataTransfer.
-// format for storing and transporting DataTransfer.
-//json is often used when data is sent from a server to web.
