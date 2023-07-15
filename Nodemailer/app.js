ab406b7e853853eebf513e8101b09980bc7132d3
RajAditya01 <adityavishal903@gmail.com>
2023-07-15 12:35:39 +0530

    EXPRESS

---
+++ Nodemailer/app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const PORT = 5500;
+
+app.get("/", (req, res) =>{
+    res.send("I am server...");
+});
+
+const start = async () => {
+    try {
+        app.listen(PORT, () => {
+            console.log(`I am live on port ${PORT}`);
+        });
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+start();
+
+// const express = require("express");
+// const app = express();
+// let PORT=5500;
+
+// app.get("/", (req, res) =>{
+//     res.send("I am server...");
+// });
+
+// const start =async() =>{
+//     try{
+//         app.listen(PORT,()=>{
+//             console.log(`I am live in port no. 5500 ${PORT}`);
+//         });
+//     }catch(error){}
+// };
