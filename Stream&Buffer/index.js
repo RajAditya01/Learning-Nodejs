62018b61b74212b1c621e28e7697cf790aa7cc2a
RajAditya01 <adityavishal903@gmail.com>
2023-07-15 11:49:58 +0530

    .

---
+++ Stream&Buffer/index.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const http =require("http");
+
+const server = http.createServer();
+
+server.on('request' , (req , res) =>{
+    // var fs =require('fs');
+    // fs.readFile('input.txt', (err, data)=>{
+    //     if(err) return console.error(err);
+    //     res.end(data.toString());
+    // });
+
+    //2nd Way
+    //Reading from a stream
+    //create a readable stream
+    //Handle stream event --> data, end , and error
+
+    const rstream = fs.createReadStream("input.txt");
+    rstream.on("data",(chunkdata)=>{
+        res.write(chunkdata);
+    });
+    rstream.on("end", ()=>{
+        res.end();
+    });
+    rstream.on("error", (err)=>{
+        console.log(err);
+        res.end("file not found");
+    });
+});
+
+server.listen(5500, "127.0.0.1");
